af26bbb0d4e1a6c36aeead0da51fab6910e43408
elmooe <elmo.erla@live.fi>
2022-12-08 17:42:10 +0200

    ExerciseActivity ja aika vitusti kaikkee muutaki, toivottavasti tiejän koneet ei levii ku lataatte

---
+++ app/src/main/java/fi/nikitanoss/hytejengi_project/Model.java
@@ -0,0 +1,67 @@
+package fi.nikitanoss.hytejengi_project;
+
+import java.util.ArrayList;
+
+public class Model {
+    String page;
+    String per_page;
+    String total;
+    String total_pages;
+    ArrayList<data> data;
+
+    public ArrayList<Model.data> getData() {
+        return data;
+    }
+
+    public void setData(ArrayList<Model.data> data) {
+        this.data = data;
+    }
+
+    public class data {
+        String id;
+        String email;
+        String first_name;
+        String last_name;
+        String avatar;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getFirst_name() {
+            return first_name;
+        }
+
+        public void setFirst_name(String first_name) {
+            this.first_name = first_name;
+        }
+
+        public String getLast_name() {
+            return last_name;
+        }
+
+        public void setLast_name(String last_name) {
+            this.last_name = last_name;
+        }
+
+        public String getAvatar() {
+            return avatar;
+        }
+
+        public void setAvatar(String avatar) {
+            this.avatar = avatar;
+        }
+    }
+}
