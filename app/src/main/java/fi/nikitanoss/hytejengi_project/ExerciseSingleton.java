7d56c78a4407a892b6e4cf468ba3e49be02536ee
NickNoss <nnossenko@gmail.com>
2022-12-12 12:18:27 +0200

    MainActivity repaired + timer onResume/onPause created Settings
    Exercise Class/Exercise Singleton
    Exercise Activity updated
    RecyclerView

--- app/src/main/java/fi/nikitanoss/hytejengi_project/ExerciseSingleton.java
+++ app/src/main/java/fi/nikitanoss/hytejengi_project/ExerciseSingleton.java
@@ -17,8 +17,7 @@ public class ExerciseSingleton {
                 "Repeat."));
         exercises.add(new Exercise("Abdominal crunch", 15, "Lie down on your back. Plant your feet on the floor, hip-width apart. Bend your knees and place your arms across your chest. Contract your abs and inhale.\n" +
                 "Exhale and lift your upper body, keeping your head and neck relaxed.\n" +
-                "Inhale and return to the starting position.\n" +
-                "26.9.2019"));
+                "Inhale and return to the starting position.\n"));
         exercises.add(new Exercise("Body-weight squat", 15, "Stand with your hands on the back of your head and your feet shoulder-width apart with your feet turned out slightly to open the hip joint.\n" +
                 "Lower your body until your thighs are parallel to the floor.\n" +
                 "Pause, then return to the starting position.\n" +
@@ -26,12 +25,12 @@ public class ExerciseSingleton {
         exercises.add(new Exercise("Bicycle crunch", 25, "Lie on your back and bring your legs to a tabletop position. Bend your elbows, and put your hands behind your head.\n" +
                 "Crunch up and bring your right elbow to your left knee, straightening your right leg.\n" +
                 "Release the crunch slightly. Bend your right leg and straighten your left leg, then bring your left elbow to your right knee.\n" +
-                "Repeat for the desired number of reps."));
+                "Repeat."));
         exercises.add(new Exercise("Burpee", 10, "Start by standing upright with your feet shoulder-width apart and your arms down at your sides.\n" +
                 "With your hands out in front of you, start to squat down. When your hands reach the ground, pop your legs straight back into a pushup position.\n" +
                 "Jump your feet up to your palms by hinging at the waist. Get your feet as close to your hands as you can get, landing them outside your hands if necessary.\n" +
                 "Stand up straight, bringing your arms above your head, and jump.\n" +
-                "This is one rep. Complete 3 sets of 10 reps as a beginner."));
+                "This is one rep."));
     }
 
     public ArrayList<Exercise> getExercises(){ return this.exercises; }
