cbb4daca1ac3564da6c8a11205bc5c8c38555a33
NickNoss <nnossenko@gmail.com>
2022-12-13 23:19:34 +0200

    Updated JavaDoc

--- app/src/main/java/fi/nikitanoss/hytejengi_project/Exercise.java
+++ app/src/main/java/fi/nikitanoss/hytejengi_project/Exercise.java
@@ -11,27 +11,46 @@ public class Exercise {
     private String name, instructions;
     private int reps;
 
+    /**
+     * Constructor for Exercise class
+     * @param name
+     */
     public Exercise(String name) { this.name = name; }
 
     /**
      * Constructor for Exercise class
+     * @param name
+     * @param reps
+     * @param instructions
      */
-
     public Exercise(String name, int reps, String instructions) {
         this.name = name;
         this.reps = reps;
         this.instructions = instructions;
     }
+
     /**
-     * Getter methods for exercise class
+     * Getter for name
+     * @return String name
      */
     public String getName() { return "Name: " + name; }
 
+    /**
+     * ToString method.
+     * @return name and number of reps
+     */
     public String toString() { return name + "\nReps: " + reps; }
 
+    /**
+     * Getter for reps.
+     * @return int reps
+     */
     public String getReps() { return "Reps: " + Integer.toString(reps); }
 
+    /**
+     * Getter for instructions.
+     * @return String instructions
+     */
     public String getInstructions() { return instructions; }
-
 
 }
